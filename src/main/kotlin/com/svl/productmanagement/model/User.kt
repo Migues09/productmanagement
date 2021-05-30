c8a71cf4abb2d029f0069a61b17dff2f05febd92
Migue <migues09@gmail.com>
2021-05-30 12:28:11 +0200

    Model updated (validate user)

--- src/main/kotlin/com/svl/productmanagement/model/User.kt
+++ src/main/kotlin/com/svl/productmanagement/model/User.kt
@@ -1,11 +1,12 @@
 package com.svl.productmanagement.model
 
+import org.springframework.validation.annotation.Validated
 import javax.persistence.*
 
 @Entity
 @Table(name = "user")
-data class User(val userName : String, val pass : String, val email : String, val name : String = "",
-                val lastName : String = ""){
+data class User(val userName : String, val pass : String, val email : String,
+                val validated: Boolean, val name : String = "", val lastName : String = ""){
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     var id : Long = 0
