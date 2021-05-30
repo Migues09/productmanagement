f829d4770f75b4bb438638ae10a56ff26fda5f47
Migue <migues09@gmail.com>
2021-05-30 09:40:32 +0200

    Initial commit

---
+++ src/main/kotlin/com/svl/productmanagement/ProductmanagementApplication.kt
@@ -0,0 +1,11 @@
+package com.svl.productmanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProductmanagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProductmanagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/svl/productmanagement/ProductmanagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.svl.productmanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProductmanagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
